aad16ff49aaab015f7b8169a444e9366a7fb20a7
MineJoanna <khayablack02@gmail.com>
2022-09-13 13:04:38 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char s1[8] = "_putchar";
+		putchar(s1[8]);
+		putchar('\n');
+
+	return (0);
+}
